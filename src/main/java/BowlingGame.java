83f09fc744e99af04c1a0903f2b18623587b4a3d
maqing <mr.maqing@gmail.com>
2020-08-03 15:37:03 +0800

    [maqing]can find errors with input

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,7 @@
+public class BowlingGame {
+    int calculateTotalScore(int[] bowlingGameScoreRecord) {
+        if (bowlingGameScoreRecord.length != 21)
+            throw new RuntimeException("There is something with bowling game score record!");
+        return 0;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class BowlingGameTest {
+
+    BowlingGame bowlingGame = new BowlingGame();
+
+    @Test
+    void should_throw_exception_when_the_length_of_input_array_is_not_21(){
+        int[] bowlingGameScoreRecord = {};
+        assertThrows(RuntimeException.class, () ->{
+            bowlingGame.calculateTotalScore(bowlingGameScoreRecord);
+        });
+    }
 }
