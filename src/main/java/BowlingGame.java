d110e04c2cb898d51f3c6a33ce91b0db1d4e391d
maqing <mr.maqing@gmail.com>
2020-08-03 18:18:00 +0800

    [maqing] should calcuate the corect total score and handle with the String input

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BowlingGame {
 
     private final int maxThrowNumber = 21;
@@ -6,20 +10,62 @@ public class BowlingGame {
     private final int maxScoreOfOneThrow = 10;
     private final int minScoreOfOneThrow = 0;
 
-    int calculateTotalScore(int[] bowlingGameScoreRecord) {
-        if (bowlingGameScoreRecordCheck(bowlingGameScoreRecord) == false)
-            throw new RuntimeException("There is something error with bowling game score record!");
-        return 0;
+    int calculateTotalScore(String bowlingGameScoreRecord) {
+        int[] bowlingGameScoreRecordArray = transferStringToArray(bowlingGameScoreRecord);
+        return calculateTotalScore(bowlingGameScoreRecordArray);
     }
 
-    boolean bowlingGameScoreRecordCheck(int[] bowlingGameScoreRecord) {
-        if (bowlingGameScoreRecord.length != maxThrowNumber) return false;
+    private int[] transferStringToArray(String bowlingGameScoreRecord) {
+        if (bowlingGameScoreRecord.length() == 0)
+            throw new RuntimeException("There is something error with bowling game score record!");
+        String[] scoreStringList = bowlingGameScoreRecord.split(",");
+        List<Integer> scoreList = new ArrayList<>();
+        try {
+            for (String scoreString : scoreStringList) {
+                scoreList.add(Integer.parseInt(scoreString.trim()));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("There is something error with bowling game score record!");
+        }
+        return scoreList.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    private int calculateTotalScore(int[] bowlingGameScoreRecord) {
+        if (bowlingGameScoreRecordCheck(bowlingGameScoreRecord) == false)
+            throw new RuntimeException("There is something error with bowling game score record!");
+        int totalScore = 0;
+        int index = 0;
+        while (index < throwNumberExceptFinalFrame) {
+            totalScore += bowlingGameScoreRecord[index] + bowlingGameScoreRecord[index + 1];
+            if (bowlingGameScoreRecord[index] == 10) {
+                totalScore += calculateScoreWhenSpike(bowlingGameScoreRecord, index);
+            } else if (bowlingGameScoreRecord[index] + bowlingGameScoreRecord[index + 1] == 10) {
+                totalScore += bowlingGameScoreRecord[index + 2];
+            }
+            index += throwNumberInOneFrameExceptionFinalFrame;
+        }
+        totalScore += bowlingGameScoreRecord[throwNumberExceptFinalFrame] + bowlingGameScoreRecord[throwNumberExceptFinalFrame + 1] + bowlingGameScoreRecord[throwNumberExceptFinalFrame + 2];
+        return totalScore;
+    }
+
+    private int calculateScoreWhenSpike(int[] bowlingGameScoreRecord, int index) {
+        int result = bowlingGameScoreRecord[index + throwNumberInOneFrameExceptionFinalFrame];
+        if (index != throwNumberExceptFinalFrame - throwNumberInOneFrameExceptionFinalFrame)
+            result += bowlingGameScoreRecord[index + throwNumberInOneFrameExceptionFinalFrame] == 10 ? bowlingGameScoreRecord[index + throwNumberInOneFrameExceptionFinalFrame * 2] : bowlingGameScoreRecord[index + throwNumberInOneFrameExceptionFinalFrame + 1];
+        else result += bowlingGameScoreRecord[index + throwNumberInOneFrameExceptionFinalFrame + 1];
+        return result;
+    }
+
+    private boolean bowlingGameScoreRecordCheck(int[] bowlingGameScoreRecord) {
+        if (bowlingGameScoreRecord.length != maxThrowNumber)
+            return false;
         for (int index = 0; index < throwNumberExceptFinalFrame / throwNumberInOneFrameExceptionFinalFrame; index++) {
             if (bowlingGameScoreRecord[index * throwNumberInOneFrameExceptionFinalFrame] + bowlingGameScoreRecord[index * throwNumberInOneFrameExceptionFinalFrame + 1] > 10)
                 return false;
         }
         for (int bowlingGameScore : bowlingGameScoreRecord) {
-            if (bowlingGameScore > maxScoreOfOneThrow || bowlingGameScore < minScoreOfOneThrow) return false;
+            if (bowlingGameScore > maxScoreOfOneThrow || bowlingGameScore < minScoreOfOneThrow)
+                return false;
         }
         for (int index = 0; index < throwNumberExceptFinalFrame / throwNumberInOneFrameExceptionFinalFrame; index++) {
             if (bowlingGameScoreRecord[index * throwNumberInOneFrameExceptionFinalFrame] + bowlingGameScoreRecord[index * throwNumberInOneFrameExceptionFinalFrame + 1] > 10)
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -8,7 +8,8 @@ public class BowlingGameTest {
 
     @Test
     void should_throw_exception_when_the_length_of_input_array_is_not_21() {
-        int[] bowlingGameScoreRecord = {};
+        // int[] bowlingGameScoreRecord = {};
+        String bowlingGameScoreRecord = "";
         assertThrows(RuntimeException.class, () -> {
             bowlingGame.calculateTotalScore(bowlingGameScoreRecord);
         });
@@ -16,10 +17,12 @@ public class BowlingGameTest {
 
     @Test
     void should_throw_exception_when_any_game_score_record_is_larger_than_10_or_negative() {
-        int[] bowlingGameScoreRecord1 = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 11, 0, 0};
-        int[] bowlingGameScoreRecord2 = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, -1, 0, 0};
-        int[][] bowlingGameScoreRecordList = {bowlingGameScoreRecord1, bowlingGameScoreRecord2};
-        for (int[] bowlingGameScoreRecord : bowlingGameScoreRecordList) {
+        // int[] bowlingGameScoreRecord1 = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 11, 0, 0};
+        // int[] bowlingGameScoreRecord2 = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, -1, 0, 0};
+        String bowlingGameScoreRecord1 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 11, 0, 0";
+        String bowlingGameScoreRecord2 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, -1, 0, 0";
+        String[] bowlingGameScoreRecordList = {bowlingGameScoreRecord1, bowlingGameScoreRecord2};
+        for (String bowlingGameScoreRecord : bowlingGameScoreRecordList) {
             assertThrows(RuntimeException.class, () -> {
                 bowlingGame.calculateTotalScore(bowlingGameScoreRecord);
             });
@@ -28,8 +31,10 @@ public class BowlingGameTest {
 
     @Test
     void should_throw_exception_when_hit_more_than_ten_pins_in_a_frame_expect_frame_ten() {
-        int[] bowlingGameScoreRecord1 = {10, 1, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 0};
-        int[] bowlingGameScoreRecord2 = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10};
+        // int[] bowlingGameScoreRecord1 = {10, 1, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 0};
+        // int[] bowlingGameScoreRecord2 = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10};
+        String bowlingGameScoreRecord1 = "10, 1, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 0";
+        String bowlingGameScoreRecord2 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10";
         assertThrows(RuntimeException.class, () -> {
             bowlingGame.calculateTotalScore(bowlingGameScoreRecord1);
         });
@@ -40,7 +45,8 @@ public class BowlingGameTest {
 
     @Test
     void should_throw_exception_when_does_not_spike_and_hit_more_than_ten_pins_in_final_frame_second_throw() {
-        int[] bowlingGameScoreRecord = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 6, 0};
+        // int[] bowlingGameScoreRecord = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 6, 0};
+        String bowlingGameScoreRecord = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 6, 0";
         assertThrows(RuntimeException.class, () -> {
             bowlingGame.calculateTotalScore(bowlingGameScoreRecord);
         });
@@ -48,9 +54,20 @@ public class BowlingGameTest {
 
     @Test
     void should_throw_exception_when_has_third_throw_in_final_frame_if_did_not_spike_or_spare() {
-        int[] bowlingGameScoreRecord = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 4, 1};
+        // int[] bowlingGameScoreRecord = {10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 4, 1};
+        String bowlingGameScoreRecord = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 4, 1";
         assertThrows(RuntimeException.class, () -> {
             bowlingGame.calculateTotalScore(bowlingGameScoreRecord);
         });
     }
+
+    @Test
+    void should_calcate_the_correct_total_score() {
+        // int[] bowlingGameScoreRecord = {5, 5, 10, 0, 10, 0, 5, 5, 8, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10};
+        String bowlingGameScoreRecord = "5, 5, 10, 0, 10, 0, 5, 5, 8, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10";
+        int exceptedScore = 241;
+        assertEquals(exceptedScore, bowlingGame.calculateTotalScore(bowlingGameScoreRecord));
+    }
+
+
 }
