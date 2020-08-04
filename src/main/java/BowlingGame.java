3db420cd401249d4b52e252fef2a2f70ff1546a1
maqing <mr.maqing@gmail.com>
2020-08-04 22:57:42 +0800

    [maqing]can check if transfered int array legal and return correct total score

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class BowlingGame {
 
+    private int totalThrowNumber = 21;
+    private int maxPinsDownNumberInAThrow = 10;
+    private int minPinsDownNumberInAThrow = 0;
+    private int totalFrameNumber = 10;
+    private int maxPinsDownNumberInAFrameBeforeFrameTen = 10;
+
     int[] inputTransfer(String pinsDownInALine) throws Exception {
         if (!isInputStringLegal(pinsDownInALine)) {
             throw new Exception("Input String formation is illegal!");
@@ -19,8 +25,78 @@ public class BowlingGame {
         if (pinsDownInALine == null || pinsDownInALine.length() == 0)
             return false;
         for (char ch : pinsDownInALine.toCharArray()) {
-            if (ch != '/' && ch < '0' || ch > '9') return false;
+            if (ch != '/' && ch != ',' && ch != ' ' && ch < '0' || ch > '9') return false;
         }
         return true;
+    }
+
+    boolean isTransferedIntArrayLegal(int[] pinsDownInALine) {
+        boolean lengthCheck = isTheLengthOfTransferedIntArrayCorrect(pinsDownInALine);
+        boolean numberRangeCheck = isTheNumberRangeOfTheTransferedIntArrayCorrect(pinsDownInALine);
+        boolean pinsDownNumberBeforeFrameTenCheck = isThePinsDownNumberCorrectBeforeFrameTenCheck(pinsDownInALine);
+        boolean pinsDownNumberInFrameTenCheck = isThePinsDownNumberCorrectInFrameTenCheck(pinsDownInALine);
+        return lengthCheck && numberRangeCheck && pinsDownNumberBeforeFrameTenCheck && pinsDownNumberInFrameTenCheck;
+    }
+
+    private boolean isTheLengthOfTransferedIntArrayCorrect(int[] pinsDownInALine) {
+        if (pinsDownInALine.length == totalThrowNumber) return true;
+        return false;
+    }
+
+    private boolean isTheNumberRangeOfTheTransferedIntArrayCorrect(int[] pinsDownInALine) {
+        for (int number : pinsDownInALine) {
+            if (number > maxPinsDownNumberInAThrow || number < minPinsDownNumberInAThrow) return false;
+        }
+        return true;
+    }
+
+    private boolean isThePinsDownNumberCorrectBeforeFrameTenCheck(int[] pinsDownInALine) {
+        for (int frame = 1; frame < totalFrameNumber; frame++) {
+            if (pinsDownInALine[frame * 2 - 2] + pinsDownInALine[frame * 2 - 1] > maxPinsDownNumberInAFrameBeforeFrameTen)
+                return false;
+        }
+        return true;
+    }
+
+    private boolean isThePinsDownNumberCorrectInFrameTenCheck(int[] pinsDownInALine) {
+        int firstThrowIndexInFrameTen = totalFrameNumber * 2 - 2;
+        int secondThrowIndexInFrameTen = firstThrowIndexInFrameTen + 1;
+        int thirdThrowIndexInFrameTen = secondThrowIndexInFrameTen + 1;
+        if (pinsDownInALine[firstThrowIndexInFrameTen] < 10 && pinsDownInALine[firstThrowIndexInFrameTen] + pinsDownInALine[secondThrowIndexInFrameTen] > 10)
+            return false;
+        if (pinsDownInALine[firstThrowIndexInFrameTen] + pinsDownInALine[secondThrowIndexInFrameTen] < 10 && pinsDownInALine[thirdThrowIndexInFrameTen] > 0)
+            return false;
+        return true;
+    }
+
+    int calculateTotalScore(String pinsDownInALine) throws Exception {
+        int[] pinsDownInALineArray = inputTransfer(pinsDownInALine);
+        if (!isTransferedIntArrayLegal(pinsDownInALineArray))
+            throw new Exception("There is something error with the pins number record!");
+        int totalScore = 0;
+        int throwIndex = 0;
+        int firstThrowIndexInFrameTen = totalFrameNumber * 2 - 2;
+        int secondThrowIndexInFrameTen = firstThrowIndexInFrameTen + 1;
+        int thirdThrowIndexInFrameTen = secondThrowIndexInFrameTen + 1;
+        while (throwIndex < firstThrowIndexInFrameTen) {
+            totalScore += pinsDownInALineArray[throwIndex] + pinsDownInALineArray[throwIndex + 1];
+            if (pinsDownInALineArray[throwIndex] == maxPinsDownNumberInAThrow) {
+                totalScore += calculateScoreWhenSpike(pinsDownInALineArray, throwIndex);
+            } else if (pinsDownInALineArray[throwIndex] + pinsDownInALineArray[throwIndex + 1] == 10) {
+                totalScore += pinsDownInALineArray[throwIndex + 2];
+            }
+            throwIndex += 2;
+        }
+        totalScore += pinsDownInALineArray[firstThrowIndexInFrameTen] + pinsDownInALineArray[secondThrowIndexInFrameTen] + pinsDownInALineArray[thirdThrowIndexInFrameTen];
+        return totalScore;
+    }
+
+    private int calculateScoreWhenSpike(int[] pinsDownInALineArray, int throwIndex) {
+        int frameScore = pinsDownInALineArray[throwIndex + 2];
+        if (throwIndex != (totalFrameNumber - 1) * 2)
+            frameScore += pinsDownInALineArray[throwIndex + 2] == 10 ? pinsDownInALineArray[throwIndex + 2 * 2] :
+                    pinsDownInALineArray[throwIndex + 2 + 1];
+        else frameScore += pinsDownInALineArray[throwIndex + 2 + 1];
+        return frameScore;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -3,15 +3,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameTest {
 
     BowlingGame bowlingGame;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         bowlingGame = new BowlingGame();
     }
 
@@ -30,4 +29,44 @@ public class BowlingGameTest {
         assertEquals(Arrays.equals(excepted3, bowlingGame.inputTransfer(pinsDownInALine3)), true);
     }
 
+    @Test
+    void shouldCheckTransferedIntArray() {
+        String pinsDownInALine0 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0";
+        String pinsDownInALine1 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 11, 0, 0";
+        String pinsDownInALine2 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, -1, 0, 0";
+        String pinsDownInALine3 = "10, 1, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 0";
+        String pinsDownInALine4 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 6, 0";
+        String pinsDownInALine5 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 5, 4, 1";
+        String pinsDownInALine6 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10";
+        String[] bowlingGameScoreRecordList = {pinsDownInALine0, pinsDownInALine1, pinsDownInALine2, pinsDownInALine3
+                , pinsDownInALine4, pinsDownInALine5};
+        for (String bowlingGameScoreRecord : bowlingGameScoreRecordList) {
+            assertThrows(Exception.class, () -> {
+                bowlingGame.calculateTotalScore(bowlingGameScoreRecord);
+            });
+        }
+        assertDoesNotThrow(() -> {
+            bowlingGame.calculateTotalScore(pinsDownInALine6);
+        });
+    }
+
+    @Test
+    void shouldReturnCorrectTotalScore() throws Exception {
+        String pinsDownInALine0 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine0),300);
+        String pinsDownInALine1 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine1),0);
+        String pinsDownInALine2 = "8, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine2),20);
+        String pinsDownInALine3 = "6, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine3),13);
+        String pinsDownInALine4 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 2, 3";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine4),15);
+        String pinsDownInALine5 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 2, 3";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine5),13);
+        String pinsDownInALine6 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine6),8);
+        String pinsDownInALine7 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 10, 2, 0";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine7),32);
+    }
 }
