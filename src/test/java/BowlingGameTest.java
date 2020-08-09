625717f3c66a211137a2e25878e192921d8bbd6e
maqing <mr.maqing@gmail.com>
2020-08-09 21:23:05 +0800

    [maqing]should return correct total socre when continuous spike happened

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -29,26 +29,6 @@ public class BowlingGameTest {
         assertEquals(Arrays.equals(excepted3, bowlingGame.inputTransfer(pinsDownInALine3)), true);
     }
 
-//    @Test
-//    void shouldReturnCorrectTotalScore() throws Exception {
-//        String pinsDownInALine0 = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine0),300);
-//        String pinsDownInALine1 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine1),0);
-//        String pinsDownInALine2 = "8, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine2),20);
-//        String pinsDownInALine3 = "6, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine3),13);
-//        String pinsDownInALine4 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 2, 3";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine4),15);
-//        String pinsDownInALine5 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 2, 3";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine5),13);
-//        String pinsDownInALine6 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine6),8);
-//        String pinsDownInALine7 = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 10, 2, 0";
-//        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine7),32);
-//    }
-
     @Test
     void shouldThrowExceptionWhenTheLengthOfTransferedIntArrayIncorrect(){
         String pinsDownInALine = "10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0";
@@ -139,5 +119,11 @@ public class BowlingGameTest {
     void shouldReturnCorrectTotalScoreWWhenSpareInFrameTen() throws Exception {
         String pinsDownInALine = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 2, 3";
         assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine),13);
+    }
+
+    @Test
+    void shouldReturnCorrectTotalScoreWWhenContinuousSpike() throws Exception {
+        String pinsDownInALine = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 10, 2, 0";
+        assertEquals(bowlingGame.calculateTotalScore(pinsDownInALine),32);
     }
 }
